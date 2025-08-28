46b1ff41acad458622556681350ac42e428077e2
aakash120608r <aakash120608r@gmail.com>
2025-08-28 08:46:44 +0530

    fix: fixed connecting errors

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 import mysql.connector
 
-def connection():
+try:
     username = input("Enter MySQL username: ")
     password = input("Enter MySQL password: ")    
+    port = int(input("Enter MySQL port number (default 3306): "))
+    if not port:
+        port = 3306
 
-    try:
-        port = int(input("Enter MySQL port number (default 3306): "))
-        if not port:
-            port = 3306
-        conn = mysql.connector.connect(
-            host='localhost',
-            user=username,
-            password=password,
-            port=port,
-            database='crickstatz'
+    conn = mysql.connector.connect(
+        host='localhost',
+        user=username,
+        password=password,
+        port=port,
+        database='crickstatz',
+        auth_plugin='mysql_native_password'
         )
-        if conn.is_connected():
-            print("Connected to CrickStatz database successfully.")
-            return conn
-        else:
-            print("Failed to connect to the database.")
-            return None
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return None
+    if conn.is_connected():
+        print("Connected to CrickStatz database successfully.")
+    else:
+        print("Failed to connect to the database.")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
