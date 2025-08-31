65f276575ad4b3c8741dd9651b78f76e33463536
aakash120608r <aakash120608r@gmail.com>
2025-08-31 13:06:15 +0530

    fix:caching_sha2_password error fixed

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 import mysql.connector
 
-try:
-    username = input("Enter MySQL username: ")
-    password = input("Enter MySQL password: ")    
-    port = int(input("Enter MySQL port number (default 3306): "))
-    if not port:
-        port = 3306
+def connect_to_database():
+    try:
+        username = input("Enter MySQL username: ")
+        password = input("Enter MySQL password: ")    
+        port = int(input("Enter MySQL port number (default 3306): "))
+        if not port:
+            port = 3306
 
-    conn = mysql.connector.connect(
-        host='localhost',
-        user=username,
-        password=password,
-        port=port,
-        database='crickstatz',
-        auth_plugin='mysql_native_password'
-        )
-    if conn.is_connected():
-        print("Connected to CrickStatz database successfully.")
-    else:
-        print("Failed to connect to the database.")
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
+        connection = mysql.connector.connect(
+            host='localhost',
+            user=username,
+            password=password,
+            port=port,
+            database='crickstatz',
+            auth_plugin='mysql_native_password'
+            )
+        if connection.is_connected():
+            print("Connected to CrickStatz database successfully.")
+        else:
+            print("Failed to connect to the database.")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+conn = connect_to_database()
